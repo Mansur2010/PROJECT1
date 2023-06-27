b2d6959424dc3d0ed4d32d5445980bb44330ca64
Mansur2010 <124903584+Mansur2010@users.noreply.github.com>
2023-06-27 20:56:01 +0600

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -1,41 +1,57 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
-OLX ='https://www.olx.kz/'
-headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"}
+ACER = "https://www.olx.kz/d/obyavlenie/noutbuk-acer-v-otlichnom-sostoyanii-s-garantiey-IDnZ7Ew.html"
+ASUS = "https://www.olx.kz/d/obyavlenie/asus-tuf-gaming-gtx1050-ryzen-3550h-IDnZgl2.html?isPreviewActive=0&sliderIndex=0"
+LENOVO = "https://www.olx.kz/d/obyavlenie/noutbuk-lenovo-g50-70-IDnZ41I.html"
+
+hello = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+}
 
 def get_olx_data1(url):
-    full_page = requests.get(url, headers = headers)
+    full_page = requests.get(url, headers=hello)
     soup = BeautifulSoup(full_page.content, 'html.parser')
-    convert = soup.findAll("h6", {"class": "16v5mdi"})
+    convert = soup.find('h1', {'class': 'css-1soizd2'})
     result = []
-    for c in convert:
-        result.append(c.text)
-    if len(result) == 0:
-        result.append('NO DATA...')
+    if convert:
+        result.append(convert.text)
+    else:
+        result.append('no data')
     return result
 
 def get_olx_data2(url):
-    full_page = requests.get(url, headers = headers)
+    full_page = requests.get(url, headers=hello)
     soup = BeautifulSoup(full_page.content, 'html.parser')
-    convert = soup.findAll("p", {"class": "10bogli"})
+    convert = soup.find('h3', {'class': 'css-ddweki'})
     result = []
-    for c in convert:
-        result.append(c.text)
-    if len(result) == 0:
-        result.append('NO DATA...')
+    if convert:
+        result.append(convert.text)
+    else:
+        result.append('no data')
     return result
 
 def get_olx_data3(url):
-    full_page = requests.get(url, headers = headers)
+    full_page = requests.get(url, headers=hello)
     soup = BeautifulSoup(full_page.content, 'html.parser')
-    convert = soup.findAll("span", {"class": "3lkihg"})
+    convert = soup.find('div', {'class': 'css-bgzo2k er34gjf0'})
     result = []
-    for c in convert:
-        result.append(c.text)
-    if len(result) == 0:
-        result.append('NO DATA...')
+    if convert:
+        result.append(convert.text)
+    else:
+        result.append('no data')
     return result
-print(get_olx_data1(OLX))
-print(get_olx_data2(OLX))
-print(get_olx_data3(OLX))
+
+
+
+print(get_olx_data1(ASUS))
+print(get_olx_data2(ASUS))
+print(get_olx_data3(ASUS))
+
+print(get_olx_data1(ACER))
+print(get_olx_data2(ACER))
+print(get_olx_data3(ACER))
+
+print(get_olx_data1(LENOVO))
+print(get_olx_data2(LENOVO))
+print(get_olx_data3(LENOVO))
